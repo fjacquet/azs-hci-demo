6108e9b142f095bd3acc46529927fd6dd561cb70
Frederic Jacquet <fred.jacquet@gmail.com>
2022-11-19 20:59:51 +0100

    use variables

--- create-aks.sh
+++ create-aks.sh
@@ -1,38 +1,48 @@
 #!/usr/bin/env bash
 
-####  AKS
-az group create --name yugabytedbRG --location switzerlandwest
 
-az group create --name yugabytedbRG --location switzerlandnorth
+
+####  ressource group
+az group create --name yugabytedbRG --location switzerlandwest
+# oups ...
+
+REGION=switzerlandnorth
+SIZE=Standard_D4ds_v5
+RSGRP=aksrg
+AKSNAME=akscluster
+YBNS=dbns
+WJNS=wikijs
+PNS=monitoring
+
+az group create --name yugabytedbRG --location $REGION
+# add provider
 az provider register --namespace Microsoft.Network
 az provider register --namespace Microsoft.Compute
 az provider register --namespace Microsoft.Storage
 az provider register --namespace Microsoft.ContainerService --wait
 
+####  AKS
 az aks create \
 --resource-group yugabytedbRG \
 --name yugabytedbAKSCluster \
 --node-count 3 \
---node-vm-size Standard_D4ds_v4 \
+--node-vm-size $SIZE \
 --enable-addons monitoring \
 --generate-ssh-keys
 
 
-az aks get-credentials --resource-group yugabytedbRG --name yugabytedbAKSCluster
-
+az aks get-credentials --resource-group $RSGRP --name $AKSNAME
 kubectl get nodes
 kubectl create clusterrolebinding yb-kubernetes-dashboard --clusterrole=cluster-admin --serviceaccount=kube-system:kubernetes-dashboard --user=clusterUser
-
-az aks browse --resource-group yugabytedbRG --name yugabytedbAKSCluster
+az aks browse --resource-group $RSGRP --name $AKSNAME
 
 
 #### Yuga
-
 helm repo add yugabytedb https://charts.yugabyte.com
 helm repo update
 helm search repo yugabytedb/yugabyte --version 2.15.3
-kubectl create namespace yb-demo
-helm install yb-demo -n yb-demo yugabytedb/yugabyte \
+kubectl create namespace $YBNS
+helm install yb-demo -n $YBNS yugabytedb/yugabyte \
  --version 2.15.3 \
  --set storage.master.count=3 \
  --set storage.tserver.count=3 \
@@ -48,9 +58,8 @@ helm install yb-demo -n yb-demo yugabytedb/yugabyte \
  --set resource.tserver.limits.memory=1Gi \
  --timeout=15m
 
-kubectl get pods --namespace yb-demo
-
-kubectl get services --namespace yb-demo
+kubectl get pods --namespace $YBNS
+kubectl get services --namespace $YBNS
 
 ####  benchmark
 wget https://github.com/yugabyte/tpcc/releases/download/2.0/tpcc.tar.gz
@@ -58,32 +67,33 @@ tar -zxvf tpcc.tar.gz
 cd tpcc
 
 
-IPS=$(kubectl get service yb-master-ui  -n yb-demo --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
-./tpccbenchmark --nodes=$IPS  --create=true
-./tpccbenchmark --load=true --nodes=$IPS
-./tpccbenchmark  --nodes=$IPS  --enable-foreign-keys=true
+IPS=$(kubectl get service yb-master-ui  -n $YBNS --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
+./tpccbenchmark --nodes=$IPS --create=true
+./tpccbenchmark --nodes=$IPS --load=true
+./tpccbenchmark --nodes=$IPS --enable-foreign-keys=true
 ./tpccbenchmark --nodes=$IPS --execute=true
 
 
 #### wikijs
 helm repo add requarks https://charts.js.wiki
-
-kubectl run ysqlsh-client -it --rm  --image yugabytedb/yugabyte-client --command -- ysqlsh -h yb-tservers.yb-demo.svc.cluster.local
-
+# connect to DB
+kubectl run ysqlsh-client -it --rm  --image yugabytedb/yugabyte-client \
+  --command -- ysqlsh -h yb-tservers.$RSGRP.svc.cluster.local
+# create DB
 create database wikijs;
 
-helm upgrade --install my-release requarks/wiki -n wikijs \
+helm upgrade --install lab requarks/wiki -n $WJNS \
   --set postgresql.enabled=false \
   --set ingres.enabled=true \
-  --set externalPostgresql.databaseURL="postgresql://yugabyte@yb-tserver-service.yb-demo.svc.cluster.local:5433/wikijs?sslmode=disable"
+  --set externalPostgresql.databaseURL="postgresql://yugabyte@yb-tserver-service.$RSGRP.svc.cluster.local:5433/wikijs?sslmode=disable"
 
-podname=$(kubectl get pod -n wikijs -o jsonpath='{.items[0].metadata.name}')
-kubectl port-forward -n wikijs $podname 8081:3000
 
-kubectl scale --replicas 5 deployment my-release-wiki -n wikijs
+podname=$(kubectl get pod -n $WJNS -o jsonpath='{.items[0].metadata.name}')
+kubectl port-forward -n $WJNS $podname 8081:3000
+kubectl scale --replicas 5 deployment lab-wiki -n $WJNS
 
 #### jenkins
-helm install my-release azure-marketplace/jenkins
+helm install lab azure-marketplace/jenkins
 
 
 #### Prometheus
@@ -94,20 +104,21 @@ helm repo update
 # Search for newly installed repositories
 helm repo list
 # Create a namespace for Prometheus and Grafana resources
-kubectl create ns monitoring
+kubectl create ns $PNS
 # Install Prometheus using HELM
-helm  upgrade --install prometheus prometheus-community/kube-prometheus-stack -n monitoring --values values.yml
+helm  upgrade --install prometheus prometheus-community/kube-prometheus-stack -n $PNS --values values.yml
 # Check all resources in Prometheus Namespace
-kubectl get all -n monitoring
+kubectl get all -n $PNS
 # Port forward the Prometheus service
-kubectl port-forward -n monitoring prometheus-prometheus-kube-prometheus-prometheus-0 9090# Get the Username
-kubectl get secret -n monitoring prometheus-grafana -o=jsonpath='{.data.admin-user}' |base64 -d
+kubectl port-forward -n $PNS prometheus-prometheus-kube-prometheus-prometheus-0 9090
+# Get the Username
+kubectl get secret -n $PNS prometheus-grafana -o=jsonpath='{.data.admin-user}' |base64 -d
 # Get the Password
-kubectl get secret -n monitoring prometheus-grafana -o=jsonpath='{.data.admin-password}' |base64 -d
+kubectl get secret -n $PNS prometheus-grafana -o=jsonpath='{.data.admin-password}' |base64 -d
 # Port forward the Grafana service
-kubectl port-forward -n monitoring prometheus-grafana-5449b6ccc9-b4dv4 3000
+kubectl port-forward -n $PNS prometheus-grafana-5449b6ccc9-b4dv4 3000
 
-
+# Enable access
 sub=$(az account subscription list -o tsv |cut -f3)
-scope="$sub/resourceGroups/yugabytedbRG"
+scope="$sub/resourceGroups/$YBNS"
 az ad sp create-for-rbac --role="Monitoring Reader" --scopes=$scope
