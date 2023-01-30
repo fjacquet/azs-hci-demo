8d15651332eef9894d9c49851709da5788508e5c
Frederic Jacquet <fred@ljf.ch>
2023-01-30 22:43:06 +0100

    add longhorn

--- register-helm.sh
+++ register-helm.sh
@@ -11,6 +11,7 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo add jenkins https://charts.jenkins.io
 helm repo add jetstack https://charts.jetstack.io
+helm repo add longhorn https://charts.longhorn.io
 helm repo add nginx https://kubernetes.github.io/ingress-nginx
 helm repo add nginx-stable https://helm.nginx.com/stable
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
